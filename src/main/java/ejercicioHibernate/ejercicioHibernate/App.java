ff195461b5b2937e2f5858aeede0a52e5a24e4b2
DanielPlaton <dcp0495@gmail.com>
2021-04-21 13:23:37 +0200

    Modificacion de la conexion hibernate.cfg.xml y creacion del logger

--- src/main/java/ejercicioHibernate/ejercicioHibernate/App.java
+++ src/main/java/ejercicioHibernate/ejercicioHibernate/App.java
@@ -1,13 +1,24 @@
 package ejercicioHibernate.ejercicioHibernate;
 
+import org.apache.log4j.Logger;
+
+import controlador.MyLogger;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static Logger logger = MyLogger.crearLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	MyLogger.createLogger();
+    
+    	logger.info("Iniciando programa");
+    
+        
     }
 }
