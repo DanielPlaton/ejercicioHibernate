e50cefeda463c970dc647df16b026c7a33f6fee2
DanielPlaton <dcp0495@gmail.com>
2021-04-21 20:23:45 +0200

    Realizacion del inserEmpleado

--- src/main/java/DAO/EmpleadoDAO.java
+++ src/main/java/DAO/EmpleadoDAO.java
@@ -4,22 +4,29 @@ import java.util.List;
 
 import org.hibernate.Session;
 
+
+
 import modelo.Empleado;
 
 public class EmpleadoDAO {
 
-	
-	public static List<Empleado> getAllEmpleados(Session s) { String hQuery =
-	  "from Empleado";
-	  
-	  List<Empleado> listaEmpleados = s.createQuery(hQuery, Empleado.class)
-	  .list(); return listaEmpleados; }
-	 
+	public static List<Empleado> getAllEmpleados(Session s) {
+		String hQuery = "from Empleado";
+
+		List<Empleado> listaEmpleados = s.createQuery(hQuery, Empleado.class).list();
+		return listaEmpleados;
+	}
+
 	public static Empleado getEmpleado(Session s, int id) {
 		// s.get(Empleado.class, id);
 		Empleado e = s.get(Empleado.class, id);
 
 		return e;
 
+	}
+
+	public static void inserEmpleado(Session s, int id, Empleado e) {
+
+		s.save(e);
 	}
 }
